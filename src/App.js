d67030cfdb96966a89a763ccc35e82ca33eb97d5
dmitry.pekutko <dkefir03@gmail.com>
2023-12-10 18:19:35 +0300

    remove garbage

--- src/App.js
+++ src/App.js
@@ -44,8 +44,6 @@ const App = observer(() => {
     <div className="App">
       <Router>
         <Container maxW='8xl' bgGradient='linear(to-br, gray.200, blue.100, green.100)' minH='100vh'>
-          hello
-          {process.env.REACT_APP_API_URL}
           <VStack>
             <Flex as={'nav'} w={'100%'} alignItems={'center'}>
               <Heading>Bank</Heading>
